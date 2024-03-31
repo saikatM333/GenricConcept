7e3dc059b65c810fa11d438a8f4e5ad553c8a13a
saikatCV <saikat030703@gmail.com>
2024-03-31 17:15:17 +0530

    find the maximum out of more than three params in genric class in standard output

---
+++ GenericClassConcept/GenericClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassConcept
+{
+    public  class GenericClass<T> where T : IComparable<T>
+    {
+        public T? MaximumFinder<T>(params T[] inputs)
+        {
+
+
+            ArrayList values = new ArrayList();
+
+            foreach (T input in inputs)
+            {
+
+                values.Add(input);
+
+            }
+
+            values.Sort();
+            return (T?)values[values.Count - 1];
+
+
+
+        }
+    }
+}
